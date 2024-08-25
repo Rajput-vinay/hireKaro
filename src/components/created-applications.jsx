3a8ef7a7e166fad2d925e86b6347fe405b91d4e9
Vinay Rajput <105452265+Rajput-vinay@users.noreply.github.com>
2024-08-25 15:12:46 +0530

    Update created-applications.jsx

--- src/components/created-applications.jsx
+++ src/components/created-applications.jsx
@@ -26,7 +26,7 @@ const CreatedApplications = () => {
 
   return (
     <div className="flex flex-col gap-2">
-      {applications.map((application) => {
+      {applications?.map((application) => {
         return (
           <ApplicationCard
             key={application.id}
